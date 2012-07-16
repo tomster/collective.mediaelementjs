66ecdcaf1eb1ebe1dff0dac7ce885bc8e4e6b033
Markos Gogoulos <mgogoulos@unweb.me>
2012-07-16 19:02:40 +0300

    rm repository

--- collective/mediaelementjs/__init__.py
+++
@@ -1,5 +0,0 @@
-import zope.i18nmessageid
-MessageFactory = zope.i18nmessageid.MessageFactory("collective.mediaelementjs")
-
-def initialize(context):
-    """Initializer called when used as a Zope 2 product."""
